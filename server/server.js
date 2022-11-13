5525f5a85981094b556fdc095ab35c09d32d9d80
MichaelBehn <michaeljin139@outlook.com>
2022-11-14 01:36:31 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -251,7 +251,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 if (process.env.NODE_ENV !== 'test') {
-    server.listen(process.env.PORT || 5030, () => {
+    server.listen(process.env.PORT || 5050, () => {
         logger.info(`[LOG=SERVER] Server started on port ${process.env.PORT}`);
     });
 }
